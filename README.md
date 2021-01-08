a51f6e05df15a81a1440dd73dfa8f3a10f879c8e
seungh0 <will.seungho@gmail.com>
2021-01-08 15:04:32 +0900

    feat(ch8): 비동기 메시지 전송하기: Kafka setup

--- README.md
+++ README.md
@@ -7,3 +7,12 @@
 ### Ch3 - Spring Data (JDBC, JPA)
 
 ### Ch4 - Spring Security
+
+### Ch5 - Spring 자동-구성
+
+### Ch6 - REST 서비스 생성하기
+
+### Ch7 - REST 서비스 사용하기 (RestClient)
+
+### Ch8 - 비동기 메시지 전송하기 (JMS, RabbitMQ, Kafka)
+
---
+++ ch8-kafka/src/main/java/tacos/TacoCloudApplication.java
@@ -0,0 +1,20 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class TacoCloudApplication {
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudApplication.class, args);
+	}
+
+}
