91728f3b70e9c9a0acf3720b89a105fc82dae633
seungh0 <will.seungho@gmail.com>
2021-01-10 15:00:09 +0900

    feat(ch11): ch11 setup

---
+++ ch11/src/main/java/tacos/data/UserRepository.java
@@ -0,0 +1,10 @@
+package tacos.data;
+
+import org.springframework.data.repository.CrudRepository;
+import tacos.domain.User;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+	User findByUsername(String username);
+
+}
